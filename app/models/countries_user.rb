a573dac9da2f9b3d01443964954bec2465636aa7
alexey89m <alexeyM.ua@gmail.com>
2014-11-07 10:51:57 +0200

    add methods :visited?

--- app/models/countries_user.rb
+++ app/models/countries_user.rb
@@ -2,4 +2,24 @@ class CountriesUser < ActiveRecord::Base
   attr_accessible :country_code, :user_id
   belongs_to :country, foreign_key:"country_code"
   belongs_to :user
+  @@total_of_countries=0
+  @@count
+  def self.count_of_visited_countries(user)
+    @@count=self.where(:user_id=>user.email).size
+  end
+
+  def self.count_of_unvisited_countries(user)
+    total # получим сначала общие количество стран
+    @@total_of_countries-@@count # результатом будет количество не посещённых стран
+  end
+
+  def self.visited?(user,country_code)
+    bool=self.where("user_id= ? AND country_code=?",user.email,country_code)
+    bool.size>0 ? true : false
+  end
+
+  private
+    def self.total
+      @@total_of_countries=Country.all.size
+    end
 end
