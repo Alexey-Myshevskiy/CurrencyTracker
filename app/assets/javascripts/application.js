0875c11ef2c7ab739c0b77956ca48ad59fd41214
alexey89m <alexeyM.ua@gmail.com>
2014-11-07 17:30:04 +0200

    checked all

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,8 @@
 //= require jquery_ujs
 //= require_self
 //= require_tree .
+$(document).ready(function() {
+    $(".check_all").click(function() {
+        $('input[type="checkbox"]' ,$(this.form)).prop("checked", $(this).prop('checked'));
+    })
+})
