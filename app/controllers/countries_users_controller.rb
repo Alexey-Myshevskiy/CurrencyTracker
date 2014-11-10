cddcba5ee43d420eee5b640bb5154c0b68dcb782
alexey89m <alexeyM.ua@gmail.com>
2014-11-10 13:06:25 +0200

    correct table columns

--- app/controllers/countries_users_controller.rb
+++ app/controllers/countries_users_controller.rb
@@ -3,11 +3,15 @@ class CountriesUsersController < ApplicationController
     def create
         user=params[:countries_user][:user]
         countries=params[:country]
-        countries.each do|elm|
-		        	if !CountriesUser.visited?(current_user,elm) # проверка на существование данной записи в базе
-		        		CountriesUser.new(:user_id=>current_user,:country_code=>elm).save #? true : false #если её ещё нет, то сохраняем
-		        end
+        if !countries.nil?
+            countries.each do|elm|
+                  if !CountriesUser.visited?(current_user,elm) # проверка на существование данной записи в базе
+                    CountriesUser.new(:user_id=>current_user,:country_code=>elm).save #? true : false #если её ещё нет, то сохраняем
+                end
+            end
+            redirect_to Country
+        else
+          render text: "<html><body><h1  style='color:red;position:center'>Error!</h1></body></html>".html_safe
         end
-        redirect_to Country
     end
 end
