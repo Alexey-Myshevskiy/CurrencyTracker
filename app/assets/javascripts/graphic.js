da1fec08aa5dfbfc4423c95b4c9ce5bcb72bd7c0
alexey89m <alexeyM.ua@gmail.com>
2014-11-13 10:26:35 +0200

    all graphics work in case countries

--- app/assets/javascripts/graphic.js
+++ app/assets/javascripts/graphic.js
@@ -1,23 +1,29 @@
 /**
  * Created by alexey on 12.11.14.
  */
-
 function LineChart(data, container) {
+
     this.data = data;
     this.container = container;
     google.load("visualization", "1", {packages:["corechart"]});
-    google.setOnLoadCallback(jQuery.proxy(this.render, this));
-    alert("!");
+    $(document).ready(function() {
+        var options = {
+            title: 'Company Performance',
+            vAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+            hAxis: { ticks: [0,25,50,75,100,125,150,175] }
+        };
+        var chart = new google.visualization.AreaChart(document.getElementById(container));
+        chart.draw(google.visualization.arrayToDataTable(data), options);
+    });
 }
 
-LineChart.prototype.render = function() {
-    alert(this.container);
-    var options = {
-        title: 'Company Performance',
-        vAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
-        hAxis: { ticks: [0,25,50,75,100,125,150,175] }
-    };
-
-    var chart = new google.visualization.AreaChart(document.getElementById(this.container));
-    chart.draw(google.visualization.arrayToDataTable(this.data), options);
-};
+//LineChart.prototype.render = function() {
+//    var options = {
+//        title: 'Company Performance',
+//        vAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+//        hAxis: { ticks: [0,25,50,75,100,125,150,175] }
+//    };
+//
+//    var chart = new google.visualization.AreaChart(document.getElementById(this.container));
+//    chart.draw(google.visualization.arrayToDataTable(this.data), options);
+//};
